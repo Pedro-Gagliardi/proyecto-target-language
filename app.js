ac829f83a571a4b88b847812b5038754356cdba7
Pedro-Gagliardi <pedroegagliardi@gmail.com>
2022-06-07 21:18:31 -0300

    Agregué carpeta 'models' y extenciones npm

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
@@ -20,6 +22,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'target1language2',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
